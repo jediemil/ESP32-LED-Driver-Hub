a257428b9d2661648cf594fc63336f503a0e49ec
Emil <emil.reyier.hedin@gmail.com>
2022-04-13 02:30:33 +0200

    Working test thing

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include "../.pio/libdeps/esp32doit-devkit-v1/Adafruit NeoPixel/Adafruit_NeoPixel.h"
+
+#define LED_PIN    4
+#define NUM_LEDS 10
+
+Adafruit_NeoPixel leds(NUM_LEDS, LED_PIN, NEO_WRGB + NEO_KHZ800);
+
+
+void setup() {
+    // write your initialization code here
+    Serial.begin(115000);
+    delay(1000);
+    leds.begin();
+    leds.clear();
+}
+
+void loop() {
+
+    leds.clear();
+    delay(1000);
+    for (int i = 0; i < 360; i++) {
+        for (int light = 0; light < NUM_LEDS; light++) {
+            leds.setPixelColor(light, leds.ColorHSV(((light*360/10)+i)*360, 255, 255));
+        }
+        leds.show();
+        delay(200);
+    }
+    leds.clear();
+    delay(1000);
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds.setPixelColor(i, 0, 255, 0, 0);
+        leds.show();
+        delay(1000);
+    }
+
+    leds.clear();
+    leds.show();
+    delay(1000);
+
+    for (int i = 0; i < 10; i++) {
+        leds.clear();
+        for (int light = 0; light < NUM_LEDS; light++) {
+            if (random(2) == 1) {
+                leds.setPixelColor(light, rand());
+            }
+        }
+        leds.show();
+        delay(1000);
+    }
+}
